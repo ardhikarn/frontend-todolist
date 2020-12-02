496aa43027cd41ac891c757dd8746026c50cd674
ardhikarn <ardhikarn@gmail.com>
2020-12-02 15:21:06 +0700

    create deletemodal component

--- src/components/TaskCard/index.tsx
+++ src/components/TaskCard/index.tsx
@@ -1,25 +1,32 @@
-import { type } from 'os'
-import React from 'react'
+import { type } from "os";
+import React from "react";
 
-import TrashIcon from 'assets/svg/trash'
-import ChecklistIcon from 'assets/svg/checklist'
+import TrashIcon from "assets/svg/trash";
+import ChecklistIcon from "assets/svg/checklist";
+
+import DeleteModal from "components/DeleteModal";
 
 type Props = {
-  title: string
-}
+  title: string;
+};
 
-const TaskCard: React.FC<Props> = ({title}) => {
+const TaskCard: React.FC<Props> = ({ title }) => {
   return (
     <div className="flex justify-center items-center relative rounded shadow-lg p-4 mb-2 bg-white">
       <p className="flex-1 text-sm subpixel-antialiased tracking-wide font-bold whitespace-normal truncate">
         {title}
       </p>
       <div className="flex text-darkPurple">
-        <span><ChecklistIcon /></span>
-        <span className="w-5 h-5 ml-4 text-red-600"><TrashIcon /></span>
+        <span>
+          <ChecklistIcon />
+        </span>
+        <span className="w-5 h-5 ml-4 text-red-600">
+          <TrashIcon />
+        </span>
       </div>
+      <DeleteModal />
     </div>
-  )
-}
+  );
+};
 
-export default TaskCard
+export default TaskCard;

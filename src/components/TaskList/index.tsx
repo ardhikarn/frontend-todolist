530f716313637a6cc9a691b40748903031355ee3
ardhikarn <ardhikarn@gmail.com>
2020-12-02 14:59:42 +0700

    create taskcard component

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 
-import Logo from 'assets/svg/logo'
+import TaskCard from 'components/TaskCard'
 
 const TaskList: React.FC = () => {
   return (
     <section className="flex flex-col overflow-x-hidden overflow-y-auto h-taskList rounded">
-      <div className="bg-white text-darkPurple rounded p-4">
-        ini sebuah todo
-      </div>
+      <TaskCard title="ini adalah sebuah todo"/>
     </section>
   )
 }
